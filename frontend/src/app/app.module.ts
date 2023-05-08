d958936c90c6ae2db3a985be7c667dda2a86a5ac
Alberto <galbarack@gmail.com>
2023-05-08 14:07:46 +0200

    commit-v2-save-4

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -6,12 +6,19 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app.routing';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
-import { MatFormFieldModule } from '@angular/material/form-field';
 import { HttpClientModule } from '@angular/common/http';
+
+import { MatFormFieldModule } from '@angular/material/form-field';
+import {MatIconModule} from '@angular/material/icon';
+import { MatSelectModule } from '@angular/material/select';
+import { MatInputModule } from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
+import {MatDividerModule} from '@angular/material/divider';
 
 import { HomeComponent } from './components/home/home.component';
 import { ConfigComponent } from './components/config/config.component';
 import { LoginComponent } from './components/login/login.component';
+
 
 @NgModule({
   declarations: [
@@ -26,8 +33,13 @@ import { LoginComponent } from './components/login/login.component';
     AppRoutingModule,
     CommonModule,
     ReactiveFormsModule,
+    HttpClientModule,
     MatFormFieldModule,
-    HttpClientModule
+    MatIconModule,
+    MatSelectModule,
+    MatInputModule,
+    MatButtonModule,
+    MatDividerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- frontend/src/app/components/login/login.component.ts
+++ frontend/src/app/components/login/login.component.ts
@@ -5,40 +5,94 @@ import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 @Component({
   selector: 'login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 
 export class LoginComponent {
 
-  showLogin:string = 'login'
-  statusTheme = [{value: 'xx'}]
+  showLogin: string = 'login'
+  hide = true
+  hidePassw = true
+  hidePasswRepit = true
  
-  signupForm: FormGroup
+  loginForm: FormGroup
+  registerForm: FormGroup
+  passwordForm: FormGroup
 
   constructor(
-    private _builder: FormBuilder,
+    private _builderLogin: FormBuilder,
+    private _builderRegister: FormBuilder,
+    private _builderPassword: FormBuilder,
     private _http: HttpClient
   ){
-    this.signupForm = this._builder.group({
-      name: [""],
-      passw: ["", Validators.required]
+    this.loginForm = this._builderLogin.group({
+      loginUser: [""],
+      loginPassw: ["", Validators.required]
+    })
+
+    this.registerForm = this._builderRegister.group({
+      registerUser: [""],
+      registerPassw: ["", Validators.required],
+      registerPasswRepit: ["", Validators.required]
+    })
+
+    this.passwordForm = this._builderPassword.group({
+      emailPassword: ["", Validators.required]
     })
   }
   
-  sendForm(values: any) {
-    console.log("sendForm: ", values);
+  sendForm(values: any, type: number) {
+    if(type == 0){ // Login
+      console.log("sendForm: ", values);
 
-    let formBody = new FormData();
-    formBody.append('name', values.name)
-    formBody.append('passw', values.passw)
+      let formBody = new FormData();
+      formBody.append('loginUser', values.loginUser)
+      formBody.append('loginPassw', values.loginPassw)
 
-    this._http.post('https://jsonplaceholder.typicode.com/users', formBody, 
-    {
-      headers: {'Content-Type': 'multipart/form-data'}
-    }).subscribe({
-      next: this.resultPostForm.bind(this),
-      error: this.errorPostForm.bind(this)
-    });
+      this._http.post('https://jsonplaceholder.typicode.com/users', formBody, 
+      {
+        headers: {'Content-Type': 'multipart/form-data'}
+      }).subscribe({
+        next: this.resultPostForm.bind(this),
+        error: this.errorPostForm.bind(this)
+      });
+    }
+    else if(type == 1){ // Register
+      console.log("registerForm: ", values);
+      if(values.registerPassw === values.registerPasswRepit){
+        let formBody = new FormData();
+        formBody.append('registerUser', values.registerUser)
+        formBody.append('registerPassw', values.registerPassw)
+    
+        this._http.post('https://jsonplaceholder.typicode.com/users', formBody, 
+        {
+          headers: {'Content-Type': 'multipart/form-data'}
+        }).subscribe({
+          next: this.resultPostForm.bind(this),
+          error: this.errorPostForm.bind(this)
+        });
+      }
+      else{
+        console.error("Las contraseñas no coinciden")
+      }
+    }
+    else if(type == 2){ // Forgotten password
+      console.log("PasswordForm: ", values);
+
+      let formBody = new FormData();
+      formBody.append('emailPassword', values.emailPassword)
+
+      this._http.post('https://jsonplaceholder.typicode.com/users', formBody, 
+      {
+        headers: {'Content-Type': 'multipart/form-data'}
+      }).subscribe({
+        next: this.resultPostForm.bind(this),
+        error: this.errorPostForm.bind(this)
+      });
+    }
+    else{ 
+      console.error("ERROR al identificar la función de la petición.")
+    }
   }
 
   resultPostForm(data: any){
@@ -47,6 +101,25 @@ export class LoginComponent {
 
   errorPostForm(err: HttpErrorResponse){
     console.error("Error: ", err);
+  }
+
+  sendForgotPassw(){
+    console.log("Password");
+    this.showLogin = 'password'
+  }
+
+  goFormRegister(){
+    console.log("register");
+    this.loginForm.reset()
+    this.registerForm.reset()
+    this.showLogin = 'register'
+  }
+
+  goBackLogin(){
+    console.log("VOLVER")
+    this.loginForm.reset()
+    this.registerForm.reset()
+    this.showLogin = 'login'
   }
   
 }
